da672a1da5fa05a823cdf2624a2e3ae364cc9ff2
Arnaldo Tadeu Chioquetta <arnaldo.chioquetta@hotmail.com>
2021-12-12 21:58:43 -0300

    Classe para tabela de horários

---
+++ tbs/tbHorarios.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XeviousPlayer2.tbs
+{
+    public class tbHorarios
+    {
+        public int ID { get; set; }
+        public DateTime HorIn { get; set; }
+        public int Lista { get; set; }
+        public int Periodicidade { get; set; }
+    }
+}
